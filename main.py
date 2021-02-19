1b270df8791dbfcfbb4def62a06a32ba9278ffcb
Umang Rastogi <umang.rastogi96@gmail.com>
2021-02-18 20:17:24 -0500

    Define main script to run algos
    Checks working of RobotWorld class

---
+++ main.py
@@ -0,0 +1,12 @@
+import ast
+from sys import argv
+
+from utils.robot_world import RobotWorld
+
+
+script, robot_params = argv
+
+if __name__ == '__main__':
+    # Convert input arguments into required data types
+    robot_params = tuple(ast.literal_eval(robot_params))
+    robot_world = RobotWorld(robot_params[0], robot_params[1])
