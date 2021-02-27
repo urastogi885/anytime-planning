5ef5b0a64b201549d0c87ceec1685fe35cab05e6
umang <umang.rastogi96@gmail.com>
2021-02-27 11:31:02 -0500

    [main.py] Run explorer using os.system

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import ast
 from sys import argv
 
@@ -6,8 +7,13 @@ from utils.robot_world import RobotWorld
 
 script, robot_params = argv
 
-if __name__ == '__main__':
+EXPLORER_RUN_CMD = "cd build/ && ./explorer"
+
+
+if __name__ == "__main__":
     # Convert input arguments into required data types
     robot_params = tuple(ast.literal_eval(robot_params))
     # Initialize the robot world class
     robot_world = RobotWorld(robot_params[0], robot_params[1])
+    # Run the explorer
+    os.system(EXPLORER_RUN_CMD)
