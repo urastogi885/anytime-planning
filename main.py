be56c58bceb3349e26ecc3d667ab103bf0ddf456
umang <umang.rastogi96@gmail.com>
2021-02-28 17:10:08 -0500

    Provide check image location to explorer
    Check image is deleted after each run

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from utils.robot_world import RobotWorld
 
 script, robot_params = argv
 
-EXPLORER_RUN_CMD = "cd build/ && ./explorer"
-
 
 if __name__ == "__main__":
     # Convert input arguments into required data types
     robot_params = tuple(ast.literal_eval(robot_params))
     # Initialize the robot world class
     robot_world = RobotWorld(robot_params[0], robot_params[1])
+    EXPLORER_RUN_CMD = "cd build/ && ./explorer " + robot_world.CHECK_IMG_LOC
     # Run the explorer
     os.system(EXPLORER_RUN_CMD)
+
+    robot_world.remove_check_image()
