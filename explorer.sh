d7ad6f510af242047ddf1afd00579233208ea3c7
umang <umang.rastogi96@gmail.com>
2021-03-05 17:43:38 -0500

    Create text file folder using shell script

--- explorer.sh
+++ explorer.sh
@@ -5,6 +5,12 @@ if [ ! -d "$PWD/build/" ]; then
 fi
 # Shift to the build directory
 cd build/
+# Remove prevoius versions of the text files
+if [ -d "path/" ]; then
+    rm -rf path/
+fi
+# Create directory to store text files
+mkdir path/
 # Configure and compile the project
 cmake ..
 cmake --build .
